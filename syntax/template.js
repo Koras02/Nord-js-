fd8936ccc7bef31818f10b77aef68d55e290682c
wjdgh0727 <wjdgh0727@naver.com>
2020-07-30 22:32:35 +0900

    intial commit

---
+++ syntax/template.js
@@ -0,0 +1,14 @@
+var name = "시발";
+var letter= 'Dear '+name+'\n\nLorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '+name+' Ut enim ad minim veniam,uis nostrud exercitation ullamco laboris nisi ut '+name+' aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. '+name;
+
+var letter= `Dear ${name}
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ${name} Ut enim ad minim veniam,uis nostrud exercitation ullamco laboris nisi ut '+name+' aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ${1+1}`;
+
+console.log(letter);
+
+var letter= `Dear ${name}
+
+hi my name is looat, aoowa ${name} us slaw, Duosia wkadSDOAWDO,${name},dkasdkad,${100}`;
+
+console.log(letter);
